0b8b41f0c8316246fbc94feafdc44c936f617849
Nicolas Couture-Grenier <nicolas.couture.grenier@gmail.com>
2022-09-18 15:38:26 -0400

    correction in intArrayParser

--- src/name/ncg/CS/Parsers.java
+++ src/name/ncg/CS/Parsers.java
@@ -7,7 +7,7 @@ public class Parsers {
   public static Function<String, Integer> integerParser = (s) -> Integer.parseInt(s.trim());
   public static Function<String, Double> doubleParser = (s) -> Double.parseDouble(s.trim());
   public static Function<String, Integer[]> intArrayParser  = (s) -> {
-    String t = s.replaceAll("[", "").replaceAll("]", "");
+    String t = s.replaceAll("\\[", "").replaceAll("\\]", "");
     String[] a = t.split(",");
     Integer[] o = new Integer[a.length];
     int i = 0;
