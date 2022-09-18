8216e81945b3dfbce72699aa2dab822c5e046b47
Nicolas Couture-Grenier <nicolas.couture.grenier@gmail.com>
2022-09-18 15:24:35 -0400

    Added Integer[] parser and printer.

--- src/name/ncg/CS/Parsers.java
+++ src/name/ncg/CS/Parsers.java
@@ -4,6 +4,16 @@ import java.util.function.Function;
 
 public class Parsers {
   public static Function<String, String> stringParser = (s) -> s;
-  public static Function<String, Integer> integerParser = (s) -> Integer.parseInt(s);
-  public static Function<String, Double> doubleParser = (s) -> Double.parseDouble(s);
+  public static Function<String, Integer> integerParser = (s) -> Integer.parseInt(s.trim());
+  public static Function<String, Double> doubleParser = (s) -> Double.parseDouble(s.trim());
+  public static Function<String, Integer[]> intArrayParser  = (s) -> {
+    String t = s.replaceAll("[", "").replaceAll("]", "");
+    String[] a = t.split(",");
+    Integer[] o = new Integer[a.length];
+    int i = 0;
+    for(String x : a) {
+      o[i++] = integerParser.apply(x);
+    }
+    return o;
+  };
 }
--- src/name/ncg/CS/Printers.java
+++ src/name/ncg/CS/Printers.java
@@ -2,8 +2,11 @@ package name.ncg.CS;
 
 import java.util.function.Function;
 
+import com.google.common.base.Joiner;
+
 public class Printers {
   public static Function<String, String> stringPrinter = (s) -> s;
   public static Function<Integer, String> integerPrinter = (i) -> Integer.toString(i);
   public static Function<Double, String> doublePrinter = (d) -> Double.toString(d);
+  public static Function<Integer[], String> intArrayPrinter = (a) -> "[" + Joiner.on(",").join(a) + "]";
 }
