cdae900c895db6669f2a9f64ea8200f661b18267
Nicolas Couture-Grenier <nicolas.couture.grenier@gmail.com>
2022-09-26 18:12:13 -0400

    unused import

--- src/name/ncg/Maths/DataStructures/FiniteBinaryRelation.java
+++ src/name/ncg/Maths/DataStructures/FiniteBinaryRelation.java
@@ -4,7 +4,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Collections;
-import java.util.List;
 import java.util.TreeSet;
 import java.util.function.BiPredicate;
 import java.util.function.Function;
