565ba133594326cc6c41191a8d4ae24edef34c25
Nicolas Couture-Grenier <nicolas.couture.grenier@gmail.com>
2022-10-14 23:06:17 -0400

    LowEntropy: 0.75

--- src/name/ncg/Music/RhythmPredicates/LowEntropy.java
+++ src/name/ncg/Music/RhythmPredicates/LowEntropy.java
@@ -10,7 +10,7 @@ public class LowEntropy implements Predicate<Rhythm> {
   
   @Override
   public boolean apply(Rhythm arg0) {
-    return cd.apply(arg0).not().quantize(0.25);
+    return cd.apply(arg0).not().quantize(0.75);
   }
 
 }
