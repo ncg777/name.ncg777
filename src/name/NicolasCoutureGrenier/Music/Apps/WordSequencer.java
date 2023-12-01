133301116b773224eb301b520d510cc382dc8112
Nicolas Couture-Grenier <nicolas.couture.grenier@gmail.com>
2023-12-01 16:55:58 -0500

    *word* sequencer

--- src/name/NicolasCoutureGrenier/Music/Apps/WordSequencer.java
+++ src/name/NicolasCoutureGrenier/Music/Apps/WordSequencer.java
@@ -15,10 +15,10 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-public class ChordSequencer {
+public class WordSequencer {
 
-  private JFrame frmChordSequencer;
-  private JTextField textChords;
+  private JFrame frmWordSequencer;
+  private JTextField textWords;
   private JTextField textSequence;
   private JTextField textOutput;
 
@@ -29,8 +29,8 @@ public class ChordSequencer {
     EventQueue.invokeLater(new Runnable() {
       public void run() {
         try {
-          ChordSequencer window = new ChordSequencer();
-          window.frmChordSequencer.setVisible(true);
+          WordSequencer window = new WordSequencer();
+          window.frmWordSequencer.setVisible(true);
         } catch (Exception e) {
           e.printStackTrace();
         }
@@ -41,7 +41,7 @@ public class ChordSequencer {
   /**
    * Create the application.
    */
-  public ChordSequencer() {
+  public WordSequencer() {
     initialize();
   }
 
@@ -49,63 +49,63 @@ public class ChordSequencer {
    * Initialize the contents of the frame.
    */
   private void initialize() {
-    frmChordSequencer = new JFrame();
-    frmChordSequencer.setTitle("PCS12 Sequencer");
-    frmChordSequencer.setBounds(100, 100, 450, 170);
-    frmChordSequencer.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frmWordSequencer = new JFrame();
+    frmWordSequencer.setTitle("Words Sequencer");
+    frmWordSequencer.setBounds(100, 100, 450, 170);
+    frmWordSequencer.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     SpringLayout springLayout = new SpringLayout();
-    frmChordSequencer.getContentPane().setLayout(springLayout);
+    frmWordSequencer.getContentPane().setLayout(springLayout);
     
-    JLabel lblNewLabel = new JLabel("Chords:");
-    springLayout.putConstraint(SpringLayout.NORTH, lblNewLabel, 10, SpringLayout.NORTH, frmChordSequencer.getContentPane());
-    springLayout.putConstraint(SpringLayout.WEST, lblNewLabel, 10, SpringLayout.WEST, frmChordSequencer.getContentPane());
-    springLayout.putConstraint(SpringLayout.SOUTH, lblNewLabel, 24, SpringLayout.NORTH, frmChordSequencer.getContentPane());
-    springLayout.putConstraint(SpringLayout.EAST, lblNewLabel, 86, SpringLayout.WEST, frmChordSequencer.getContentPane());
+    JLabel lblNewLabel = new JLabel("Words:");
+    springLayout.putConstraint(SpringLayout.NORTH, lblNewLabel, 10, SpringLayout.NORTH, frmWordSequencer.getContentPane());
+    springLayout.putConstraint(SpringLayout.WEST, lblNewLabel, 10, SpringLayout.WEST, frmWordSequencer.getContentPane());
+    springLayout.putConstraint(SpringLayout.SOUTH, lblNewLabel, 24, SpringLayout.NORTH, frmWordSequencer.getContentPane());
+    springLayout.putConstraint(SpringLayout.EAST, lblNewLabel, 86, SpringLayout.WEST, frmWordSequencer.getContentPane());
     lblNewLabel.setFont(new Font("DejaVu Sans Mono", Font.PLAIN, 11));
     lblNewLabel.setHorizontalAlignment(SwingConstants.RIGHT);
-    frmChordSequencer.getContentPane().add(lblNewLabel);
+    frmWordSequencer.getContentPane().add(lblNewLabel);
     
-    textChords = new JTextField();
-    springLayout.putConstraint(SpringLayout.NORTH, textChords, 7, SpringLayout.NORTH, frmChordSequencer.getContentPane());
-    springLayout.putConstraint(SpringLayout.WEST, textChords, 2, SpringLayout.EAST, lblNewLabel);
-    springLayout.putConstraint(SpringLayout.SOUTH, textChords, 27, SpringLayout.NORTH, frmChordSequencer.getContentPane());
-    springLayout.putConstraint(SpringLayout.EAST, textChords, -10, SpringLayout.EAST, frmChordSequencer.getContentPane());
-    frmChordSequencer.getContentPane().add(textChords);
-    textChords.setColumns(10);
+    textWords = new JTextField();
+    springLayout.putConstraint(SpringLayout.NORTH, textWords, 7, SpringLayout.NORTH, frmWordSequencer.getContentPane());
+    springLayout.putConstraint(SpringLayout.WEST, textWords, 2, SpringLayout.EAST, lblNewLabel);
+    springLayout.putConstraint(SpringLayout.SOUTH, textWords, 27, SpringLayout.NORTH, frmWordSequencer.getContentPane());
+    springLayout.putConstraint(SpringLayout.EAST, textWords, -10, SpringLayout.EAST, frmWordSequencer.getContentPane());
+    frmWordSequencer.getContentPane().add(textWords);
+    textWords.setColumns(10);
     
     JLabel lblNewLabel_1 = new JLabel("Sequence:");
-    springLayout.putConstraint(SpringLayout.WEST, lblNewLabel_1, 10, SpringLayout.WEST, frmChordSequencer.getContentPane());
+    springLayout.putConstraint(SpringLayout.WEST, lblNewLabel_1, 10, SpringLayout.WEST, frmWordSequencer.getContentPane());
     springLayout.putConstraint(SpringLayout.EAST, lblNewLabel_1, 0, SpringLayout.EAST, lblNewLabel);
     lblNewLabel_1.setFont(new Font("DejaVu Sans Mono", Font.PLAIN, 11));
     lblNewLabel_1.setHorizontalAlignment(SwingConstants.RIGHT);
-    frmChordSequencer.getContentPane().add(lblNewLabel_1);
+    frmWordSequencer.getContentPane().add(lblNewLabel_1);
     
     textSequence = new JTextField();
-    springLayout.putConstraint(SpringLayout.NORTH, textSequence, 11, SpringLayout.SOUTH, textChords);
+    springLayout.putConstraint(SpringLayout.NORTH, textSequence, 11, SpringLayout.SOUTH, textWords);
     springLayout.putConstraint(SpringLayout.WEST, textSequence, 2, SpringLayout.EAST, lblNewLabel_1);
-    springLayout.putConstraint(SpringLayout.EAST, textSequence, -10, SpringLayout.EAST, frmChordSequencer.getContentPane());
+    springLayout.putConstraint(SpringLayout.EAST, textSequence, -10, SpringLayout.EAST, frmWordSequencer.getContentPane());
     springLayout.putConstraint(SpringLayout.NORTH, lblNewLabel_1, 3, SpringLayout.NORTH, textSequence);
-    frmChordSequencer.getContentPane().add(textSequence);
+    frmWordSequencer.getContentPane().add(textSequence);
     textSequence.setColumns(10);
     
     JButton btnNewButton = new JButton("Sequence");
     btnNewButton.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
-        String[] chords = textChords.getText().split("\\s+");
+        String[] words = textWords.getText().split("\\s+");
         Sequence s = Sequence.parse(textSequence.getText());
         
         String o = "";
         for(int i=0;i<s.size();i++) {
-          o += chords[s.get(i)] + " ";
+          o += words[s.get(i)] + " ";
         }
         textOutput.setText(o.trim());
       }
     });
     springLayout.putConstraint(SpringLayout.NORTH, btnNewButton, 6, SpringLayout.SOUTH, textSequence);
-    springLayout.putConstraint(SpringLayout.WEST, btnNewButton, 0, SpringLayout.WEST, textChords);
+    springLayout.putConstraint(SpringLayout.WEST, btnNewButton, 0, SpringLayout.WEST, textWords);
     springLayout.putConstraint(SpringLayout.SOUTH, btnNewButton, 29, SpringLayout.SOUTH, textSequence);
-    springLayout.putConstraint(SpringLayout.EAST, btnNewButton, 0, SpringLayout.EAST, textChords);
-    frmChordSequencer.getContentPane().add(btnNewButton);
+    springLayout.putConstraint(SpringLayout.EAST, btnNewButton, 0, SpringLayout.EAST, textWords);
+    frmWordSequencer.getContentPane().add(btnNewButton);
     
     JLabel lblNewLabel_2 = new JLabel("Output:");
     springLayout.putConstraint(SpringLayout.NORTH, lblNewLabel_2, 39, SpringLayout.SOUTH, lblNewLabel_1);
@@ -113,13 +113,13 @@ public class ChordSequencer {
     springLayout.putConstraint(SpringLayout.EAST, lblNewLabel_2, 0, SpringLayout.EAST, lblNewLabel);
     lblNewLabel_2.setFont(new Font("DejaVu Sans Mono", Font.PLAIN, 11));
     lblNewLabel_2.setHorizontalAlignment(SwingConstants.RIGHT);
-    frmChordSequencer.getContentPane().add(lblNewLabel_2);
+    frmWordSequencer.getContentPane().add(lblNewLabel_2);
     
     textOutput = new JTextField();
     springLayout.putConstraint(SpringLayout.NORTH, textOutput, 6, SpringLayout.SOUTH, btnNewButton);
-    springLayout.putConstraint(SpringLayout.WEST, textOutput, 0, SpringLayout.WEST, textChords);
-    springLayout.putConstraint(SpringLayout.EAST, textOutput, 0, SpringLayout.EAST, textChords);
-    frmChordSequencer.getContentPane().add(textOutput);
+    springLayout.putConstraint(SpringLayout.WEST, textOutput, 0, SpringLayout.WEST, textWords);
+    springLayout.putConstraint(SpringLayout.EAST, textOutput, 0, SpringLayout.EAST, textWords);
+    frmWordSequencer.getContentPane().add(textOutput);
     textOutput.setColumns(10);
   }
 }
