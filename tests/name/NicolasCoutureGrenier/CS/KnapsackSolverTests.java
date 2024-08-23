50b07247e535f4953a0a9eb59f0ade0342ec452c
Nicolas Couture-Grenier <nicolas.couture.grenier@gmail.com>
2024-08-23 18:27:40 -0400

    added some tests

--- tests/name/NicolasCoutureGrenier/CS/KnapsackSolverTests.java
+++ tests/name/NicolasCoutureGrenier/CS/KnapsackSolverTests.java
@@ -62,4 +62,44 @@ public class KnapsackSolverTests extends TestCase {
         int[] solution = KnapsackSolver.solve(profits, weights, capacity, epsilon);
         assertArrayEquals(expectedSolution, solution);
     }
+    
+    @Test
+    public void testMultipleItems() {
+        int[] profits = {60, 100, 120};
+        int[] weights = {10, 20, 30};
+        int capacity = 50;
+        double epsilon = 0.01;
+        int[] expectedSolution = {0,1,1};
+        assertArrayEquals(expectedSolution, KnapsackSolver.solve(profits, weights, capacity, epsilon));
+    }
+
+    @Test
+    public void testLargeCapacity() {
+        int[] profits = {10, 20, 30, 40, 50};
+        int[] weights = {5, 10, 15, 20, 25};
+        int capacity = 100;
+        double epsilon = 0.01;
+        int[] expectedSolution = {1,1,1,1,1};
+        assertArrayEquals(expectedSolution, KnapsackSolver.solve(profits, weights, capacity, epsilon));
+    }
+
+    @Test
+    public void testSmallEpsilon() {
+        int[] profits = {10, 20, 30};
+        int[] weights = {5, 10, 15};
+        int capacity = 20;
+        double epsilon = 0.0001;
+        int[] expectedSolution = {1,0,1};
+        assertArrayEquals(expectedSolution, KnapsackSolver.solve(profits, weights, capacity, epsilon));
+    }
+
+    @Test
+    public void testDuplicateItems() {
+        int[] profits = {10, 10, 20, 20, 30};
+        int[] weights = {5, 5, 10, 10, 15};
+        int capacity = 30;
+        double epsilon = 0.01;
+        int[] expectedSolution = {1,1,1,1,0};
+        assertArrayEquals(expectedSolution, KnapsackSolver.solve(profits, weights, capacity, epsilon));
+    }
 }
