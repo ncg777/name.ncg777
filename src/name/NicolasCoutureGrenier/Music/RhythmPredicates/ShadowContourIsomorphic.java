5bc3c4b9ea75ce4d51e274ec2a946969c81fc67a
Nicolas Couture-Grenier <nicolas.couture.grenier@gmail.com>
2024-09-08 07:50:47 -0400

    ...

--- src/name/NicolasCoutureGrenier/Music/RhythmPredicates/ShadowContourIsomorphic.java
+++ src/name/NicolasCoutureGrenier/Music/RhythmPredicates/ShadowContourIsomorphic.java
@@ -1,5 +1,7 @@
 package name.NicolasCoutureGrenier.Music.RhythmPredicates;
 
+import javax.annotation.Nonnull;
+
 import name.NicolasCoutureGrenier.CS.Functional.StandardAndGuavaPredicate;
 import name.NicolasCoutureGrenier.Maths.Objects.Sequence;
 import name.NicolasCoutureGrenier.Music.Rhythm;
@@ -7,8 +9,9 @@ import name.NicolasCoutureGrenier.Music.Rhythm;
 public class ShadowContourIsomorphic implements StandardAndGuavaPredicate<Rhythm> {
 
   @Override
-  public boolean apply(Rhythm input) {
+  public boolean apply(@Nonnull Rhythm input) {
     if(input.getK()<2) return true;
-    return Sequence.equivalentUnderRotation(input.getContour(), input.getShadowContour());
+    return Sequence.equivalentUnderRotation(input.getContour(), input.getShadowContour()) || 
+        Sequence.equivalentUnderRotation(input.getContour().reverse(), input.getShadowContour());
   }
 }
