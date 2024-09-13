c3437c728438afa28817f5e46f85fa9370f908f5
Nicolas Couture-Grenier <nicolas.couture.grenier@gmail.com>
2024-09-13 07:39:02 -0400

    more javadoc

--- src/name/NicolasCoutureGrenier/Statistics/SumOfDistanceToMean.java
+++ src/name/NicolasCoutureGrenier/Statistics/SumOfDistanceToMean.java
@@ -2,7 +2,34 @@ package name.NicolasCoutureGrenier.Statistics;
 
 import org.apache.commons.math3.stat.StatUtils;
 import org.apache.commons.math3.stat.descriptive.UnivariateStatistic;
-
+/**
+ * The {@code SumOfDistanceToMean} class implements the {@code UnivariateStatistic} interface,
+ * providing a method to calculate the sum of absolute distances of values from their mean.
+ * 
+ * <p>This statistic is useful in statistical analysis to measure the dispersion of data points
+ * relative to the mean value. It essentially quantifies how far each value in a dataset is 
+ * from the average, without considering the direction of the difference.</p>
+ *
+ * <p>Key methods include:</p>
+ * <ul>
+ * <li>{@code evaluate(double[] values)}: Computes the sum of distances of the given values 
+ * from their mean.</li>
+ * <li>{@code evaluate(double[] values, int begin, int length)}: Computes the sum of distances 
+ * for a specified subarray of the given values, defined by the starting index and length.</li>
+ * <li>{@code copy()}: Returns a copy of the current statistic instance. In this case, as there 
+ * is no internal state, it simply returns the instance itself.</li>
+ * </ul>
+ *
+ * <p>Example usage:</p>
+ * <pre>
+ * double[] data = {1.0, 2.0, 3.0, 4.0};
+ * SumOfDistanceToMean statistic = new SumOfDistanceToMean();
+ * double result = statistic.evaluate(data); // Computes the sum of distances of all values from the mean
+ * </pre>
+ * 
+ * @see UnivariateStatistic
+ * @see StatUtils
+ */
 public class SumOfDistanceToMean implements UnivariateStatistic {
 
   @Override
