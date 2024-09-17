73146560493ece5dc1ef9fd8954f2658f6d0cec2
Nicolas Couture-Grenier <nicolas.couture.grenier@gmail.com>
2024-09-17 14:27:56 -0400

    .......

--- src/name/NicolasCoutureGrenier/Maths/Relations/SetRelations.java
+++ src/name/NicolasCoutureGrenier/Maths/Relations/SetRelations.java
@@ -4,6 +4,6 @@ import java.util.Set;
 import java.util.function.BiPredicate;
 
 public class SetRelations {
-  public static <T> BiPredicate<Set<T>, Set<T>> subsetOf() { return (a,b) -> b.containsAll(a); }
-  public static <T> BiPredicate<Set<T>, Set<T>> supersetOf() { return (a,b) -> a.containsAll(b); }
+  public static BiPredicate<Set<?>, Set<?>> subsetOf = (a,b) -> b.containsAll(a);
+  public static BiPredicate<Set<?>, Set<?>> supersetOf = (a,b) -> a.containsAll(b);
 }
