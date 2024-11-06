0549619d46b01252774d0a762e6a278e0e2b4f08
Nicolas Couture-Grenier <nicolas.couture.grenier@gmail.com>
2024-11-06 07:19:12 -0500

    safety commit :

--- src/name/NicolasCoutureGrenier/CS/DataStructures/TritWord32.java
+++ src/name/NicolasCoutureGrenier/CS/DataStructures/TritWord32.java
@@ -2,7 +2,37 @@ package name.NicolasCoutureGrenier.CS.DataStructures;
 
 public class TritWord32 {
     private Long storage = 0l;
-
+    
+    // UNARY OPERATORS
+    int[] BUF = {-1,0,1};
+    int[] NOT = {1,0,-1};
+    int[] PNOT = {1,1,-1};
+    int[] NNOT = {1,-1,-1};
+    int[] ABS = {1,0,1};
+    int[] CLU = {0,0,1};
+    int[] CLD = {-1,0,0};
+    int[] INC = {0,1,1};
+    int[] DEC = {-1,-1,0};
+    int[] RTU = {0,1,-1};
+    int[] RTD = {1,-1,0};
+    int[] ISP = {-1,-1,1};
+    int[] ISZ = {-1,1,-1};
+    int[] ISN = {1,-1,-1};
+    
+    // BINARY OPERATORS
+    int[][] AND = {{-1,-1,-1},{-1,0,0},{-1,0,1}};
+    int[][] NAND = {{1,1,1},{1,0,0},{1,0,-1}};
+    int[][] OR = {{-1,0,1},{0,0,1},{1,1,1}};
+    int[][] NOR = {{1,0,-1},{0,0,-1},{-1,-1,-1}};
+    int[][] CONS = {{-1,0,0},{0,0,0},{0,0,1}};
+    int[][] NCONS = {{1,0,0},{0,0,0},{0,0,-1}};
+    int[][] ANY = {{-1,-1,0},{-1,0,1},{0,1,1}};
+    int[][] NANY = {{1,1,0},{1,0,-1},{0,-1,-1}};
+    int[][] MUL = {{1,0,-1},{0,0,0},{-1,0,1}};
+    int[][] NMUL = {{-1,0,1},{0,0,0},{1,0,-1}};
+    int[][] SUM = {{1,-1,0},{-1,0,1},{0,1,-1}};
+    int[][] NSUM = {{-1,1,0},{1,0,-1},{0,-1,1}};
+    
     public TritWord32() {
         boolean is64Bit = System.getProperty("os.arch").contains("64");
         if(!is64Bit) throw new RuntimeException("Only 64 bit architectures supported");    
