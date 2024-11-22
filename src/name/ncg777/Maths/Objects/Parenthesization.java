c8822ec90772cca22771a3b861b5d540f4ef7cb0
Nicolas Couture-Grenier <nicolas.couture.grenier@gmail.com>
2024-11-22 15:35:09 +0000

    discarded broken code. moved the class. progress made

---
+++ src/name/ncg777/Maths/Objects/Parenthesization.java
@@ -0,0 +1,15 @@
+package name.ncg777.Maths.Objects;
+
+
+
+/**
+ * 
+ * P(n) = \sum_{k=0}^{n} \binom{n}{k} \cdot 2^{n-k}
+ */
+public class Parenthesization {
+
+  private static final char OPEN = '(';
+  private static final char CHAR = '□';
+  private static final char CLOSE = ')';
+
+}
