1b4b837be247d224357d12320d8aad9d586a78c4
Nicolas Couture-Grenier <nicolas.couture.grenier@gmail.com>
2024-11-23 19:13:14 +0000

    mission accomplished

--- src/name/ncg777/Maths/Objects/Parenthesization.java
+++ src/name/ncg777/Maths/Objects/Parenthesization.java
@@ -1,8 +1,7 @@
 package name.ncg777.Maths.Objects;
 
 import java.util.ArrayList;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.function.Consumer;
 
 public class Parenthesization implements Comparable<Parenthesization> {
   public enum Parenthesis {
@@ -15,96 +14,70 @@ public class Parenthesization implements Comparable<Parenthesization> {
   private static final char CLOSE = '}';
 
   private Integer nbOfCharacters = null;
-  private ArrayList<Character> innerParentheses = null;
-  private ArrayList<Character> characters = null;
+  private ArrayList<Character> innerParentheses = new ArrayList<>();
+  private ArrayList<Character> characters = new ArrayList<Character>();
   
   public Parenthesization(int nbOfCharacters) {
     super();
     this.nbOfCharacters = nbOfCharacters;
-    innerParentheses = new ArrayList<>();
     for(int i=0;i<nbOfCharacters-1;i++) innerParentheses.add(null);
-    
-    this.characters = new ArrayList<Character>();
     for(int i=0;i<nbOfCharacters;i++) characters.add(DEFAULT_CHAR);
   }
   
   public Parenthesization(String s) {
     super();
     this.nbOfCharacters = s.length();
-    innerParentheses = new ArrayList<>();
-    
     for(int i=0;i<this.nbOfCharacters-1;i++) innerParentheses.add(null);
-    
-    this.characters = new ArrayList<Character>();
-    for(int i=0;i<s.length();i++) characters.add(s.charAt(i));
-    for(int i=0;i<this.nbOfCharacters;i++) characters.add(i, DEFAULT_CHAR);
+    for(int i=0;i<this.nbOfCharacters;i++) characters.add(s.charAt(i));
   }
   
-  @SuppressWarnings("null")
+  public Parenthesization(Parenthesization p) {
+    this(p.nbOfCharacters);
+    characters.clear(); for(Character c : p.characters) characters.add(c);
+    innerParentheses.clear(); for(Character c : p.innerParentheses) innerParentheses.add(c);
+  }
+  
   public Parenthesization mutateParenthesis(int i, Parenthesis p) {
     if(i < 0 || i > this.nbOfCharacters-1) throw new IndexOutOfBoundsException();
-    
-    Parenthesization c = null;
-    try {
-      c = (Parenthesization)this.clone();
-    } catch (CloneNotSupportedException e){;}
-    if(p == Parenthesis.CLOSE) c.innerParentheses.set(i, CLOSE);
-    if(p == null) c.innerParentheses.set(i, null);
+    Parenthesization c = new Parenthesization(this);
     if(p == Parenthesis.OPEN) c.innerParentheses.set(i, OPEN);
+    if(p == null) c.innerParentheses.set(i, null);
+    if(p == Parenthesis.CLOSE) c.innerParentheses.set(i, CLOSE);
     return c;
   }
   
   public Parenthesization mutateCharacter(int i, char c) {
     if(i < 0 || i > this.nbOfCharacters) throw new IndexOutOfBoundsException();
-    Parenthesization p = null;
-    try {
-      p = ((Parenthesization)this.clone());
-      p.characters.set(i,c);
-    } catch(CloneNotSupportedException ex) {;}
-    
+    Parenthesization p = new Parenthesization(this);
+    p.characters.set(i,c);
     return p;
   }
   
-  public static Set<Parenthesization> generate(int nbOfCharacters) {
-    return generate(new TreeSet<Parenthesization>(), nbOfCharacters, new Parenthesization(nbOfCharacters), 0);
+  public static void enumerate(Consumer<String> consumer, int nbOfCharacters ) {
+    enumerate(consumer, nbOfCharacters, new Parenthesization(nbOfCharacters), 0);
   }
   
-  private static Set<Parenthesization> generate(Set<Parenthesization> set, int nbOfCharacters, Parenthesization current, int i) {
-    set.add(current);
+  private static void enumerate(Consumer<String> consumer, int nbOfCharacters, Parenthesization current, int i) {
+    consumer.accept(current.toString());
     if(i < nbOfCharacters-1) {
-      try {
-        generate(set, nbOfCharacters, ((Parenthesization)current.clone()).mutateParenthesis(i, Parenthesis.OPEN), i+1);
-        generate(set, nbOfCharacters, ((Parenthesization)current.clone()).mutateParenthesis(i, null), i+1);
-        generate(set, nbOfCharacters, ((Parenthesization)current.clone()).mutateParenthesis(i, Parenthesis.CLOSE), i+1);
-      } catch (CloneNotSupportedException e){;}
+      enumerate(consumer, nbOfCharacters, current.mutateParenthesis(i, Parenthesis.OPEN), i+1);
+      enumerate(consumer, nbOfCharacters, current.mutateParenthesis(i, null), i+1);
+      enumerate(consumer, nbOfCharacters, current.mutateParenthesis(i, Parenthesis.CLOSE), i+1);
     }
-    return set;
   }
   
   public String toString() {
-    return this.toString(false);
-  }
-  
-  public String toString(boolean useDefaultCharacter) {
-    int parenthesis_index = 0;
-    int character_index = 0;
+    int p_i = 0;
+    int c_i = 0;
     
-    boolean b = false;
     StringBuilder sb = new StringBuilder();
     sb.append(OPEN);
-    while(parenthesis_index < this.nbOfCharacters-1) {
-      if(b) {
-        if(innerParentheses.get(parenthesis_index++) != null) {
-          sb.append(innerParentheses.get(parenthesis_index));  
-        }
-      } else {
-        sb.append(useDefaultCharacter ? DEFAULT_CHAR : characters.get(character_index++));
-      }
+    while(c_i < characters.size()) {
+      sb.append(characters.get(c_i++));
       
-      if(parenthesis_index < this.innerParentheses.size() && 
-          innerParentheses.get(parenthesis_index) == null) {
-        b = !b;
-      }
+      if(p_i++ < innerParentheses.size() && innerParentheses.get(p_i-1) != null) {
+          sb.append(innerParentheses.get(p_i-1));  
+      } 
     }
     sb.append(CLOSE);
     return sb.toString();
@@ -112,6 +85,6 @@ public class Parenthesization implements Comparable<Parenthesization> {
 
   @Override
   public int compareTo(Parenthesization o) {
-    return this.toString(true).compareTo(o.toString(true));
+    return this.toString().compareTo(o.toString());
   }
 }
