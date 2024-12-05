ac948214699636e1cdb05f737228d623925c0f06
Nicolas Couture-Grenier <nicolas.couture.grenier@gmail.com>
2024-12-05 04:10:23 +0000

    ...

---
+++ src/name/ncg777/maths/sentences/Sentence.java
@@ -0,0 +1,11 @@
+package name.ncg777.maths.sentences;
+
+import java.util.ArrayList;
+
+import name.ncg777.maths.words.Word;
+
+public class Sentence<T extends Word> extends ArrayList<T> {
+
+  private static final long serialVersionUID = 1L;
+
+}
