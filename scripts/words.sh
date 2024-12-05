4492694a44cfc8da9024fb13943a739d8801f887
Nicolas Couture-Grenier <nicolas.couture.grenier@gmail.com>
2024-12-05 00:46:22 +0000

    ...

--- scripts/words.sh
+++ scripts/words.sh
@@ -1,1 +1,1 @@
-java name.ncg777.computing.ReflectiveEnumerationPrinter name.ncg777.mathematics.enumerations.WordEnumeration $@
+java name.ncg777.computing.ReflectiveEnumerationPrinter name.ncg777.maths.enumerations.WordEnumeration $@
