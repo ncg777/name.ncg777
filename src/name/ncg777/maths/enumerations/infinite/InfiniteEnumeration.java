1ca04aa62c5c8f7dcda0c1a9bca0aa1e53b0d6cf
Nicolas Couture-Grenier <nicolas.couture.grenier@gmail.com>
2024-12-23 11:32:24 -0500

    ...

--- src/name/ncg777/maths/enumerations/infinite/InfiniteEnumeration.java
+++ src/name/ncg777/maths/enumerations/infinite/InfiniteEnumeration.java
@@ -1,9 +1,14 @@
 package name.ncg777.maths.enumerations.infinite;
 
 import java.util.Enumeration;
+import java.util.function.Supplier;
 
 public interface InfiniteEnumeration<E> extends Enumeration<E> {
   default public boolean hasMoreElements() {
     return true;
+  }
+  
+  default public Supplier<E> asSupplier() {
+    return () -> this.nextElement();
   }
 }
