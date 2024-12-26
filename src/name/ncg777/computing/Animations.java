66d761567221aa7dc880c2c71c7f25d0f56b38fe
Nicolas Couture-Grenier <nicolas.couture.grenier@gmail.com>
2024-12-26 00:49:19 -0500

    ...

--- src/name/ncg777/computing/Animations.java
+++ src/name/ncg777/computing/Animations.java
@@ -29,26 +29,28 @@ public class Animations {
           for(int _i=0;_i<2;_i++) {
             final double i = list.get(_i);
             for(int _j=0;_j<2;_j++) {
-            final double j = list.get(_j);
-            GraphicsFunctions.drawParametric2D(
+              final double j = list.get(_j);
+              
+              GraphicsFunctions.drawParametric2D(
                   g, 
-                  (t) -> 0.5+j*0.4*Math.cos(j*Math.PI*0.125+i*normalized_time*40*Math.PI)*(0.5-0.5*Math.cos((4.0-(Math.abs(2.0-4.0*t)*2.0+2.0*(2.0+(1.0-Math.abs(1.0-2.0*normalized_time)))))*Math.PI)), 
-                  (t) -> 0.5-j*0.4*Math.sin(               -i*normalized_time*40*Math.PI)*(0.5+0.5*Math.sin((2.0-(Math.abs(1.0-2.0*t)*2.0+2.0*(4.0+(2.0-Math.abs(2.0-4.0*normalized_time)))))*Math.PI)),
+                  (t) -> 0.5+j*0.35*Math.cos(j*Math.PI*0.125+i*normalized_time*40*Math.PI)*(0.5-0.5*Math.cos((4.0-(Math.abs(2.0-4.0*t)*2.0+2.0*(2.0+(1.0-Math.abs(1.0-2.0*normalized_time)))))*Math.PI)), 
+                  (t) -> 0.5-j*0.35*Math.sin(               -i*normalized_time*40*Math.PI)*(0.5+0.5*Math.sin((2.0-(Math.abs(1.0-2.0*t)*2.0+2.0*(4.0+(2.0-Math.abs(2.0-4.0*normalized_time)))))*Math.PI)),
                   0.0,
                   1.0,
                   (t) -> 0.0,
                   (t) -> 0.0,
                   (t) -> Double.valueOf(width),
                   (t) -> Double.valueOf(height),
-                  (t) -> 0.05+0.05*Math.sin(42*Math.PI*t)*Math.sin(20*Math.PI*normalized_time),
+                  (t) -> 0.1+0.1*Math.sin(42*Math.PI*t)*Math.sin(20*Math.PI*normalized_time),
                   (t) -> new Color(
                       (int)Math.round(128.0-127.0*(j*Math.cos(Math.PI*(3.0*t-i*normalized_time)))), 
                       (int)Math.round(128.0+127.0*(j*Math.sin(Math.PI*(3.0*t+i*normalized_time)))), 
                       128,
-                      128
+                      16
                      ),
-                  (t) -> 0.0025+0.002*Math.sin(Math.PI*2.0*t + Math.PI*Math.sin(1.0*Math.PI*normalized_time))
+                  (t) -> 0.00075+0.0007*Math.sin(Math.PI*2.0*t + Math.PI*Math.sin(80.0*Math.PI*normalized_time))
                   );
+              g.rotate(40.0*(double)k*2*Math.PI/(double)upper, width/2.0, height/2.0);
             }
           }
           
