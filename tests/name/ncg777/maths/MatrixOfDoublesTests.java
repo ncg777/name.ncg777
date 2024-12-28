090916dfb74736001cacf1be682ebdd6b9614651
Nicolas Couture-Grenier <nicolas.couture.grenier@gmail.com>
2024-12-28 09:25:50 -0500

    tests

---
+++ tests/name/ncg777/maths/MatrixOfDoublesTests.java
@@ -0,0 +1,34 @@
+package name.ncg777.maths;
+
+import static org.hamcrest.CoreMatchers.is;
+
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import junit.framework.TestCase;
+public class MatrixOfDoublesTests extends TestCase{
+
+  private MatrixOfDoubles matrix;
+  
+  @Override
+  protected void setUp() throws Exception {
+      matrix = new MatrixOfDoubles(new Double[][]{{1.0, 2.0}, {3.0, 4.0}});
+  }
+
+  public void testProduct() {
+      MatrixOfDoubles other = new MatrixOfDoubles(new Double[][]{{5.0, 6.0}, {7.0, 8.0}});
+      MatrixOfDoubles result = matrix.product(other);
+      assertThat(result.toDoubleArray(), is(new Double[][]{{19.0, 22.0}, {43.0, 50.0}}));
+  }
+
+  public void testComputeDeterminant() {
+      Double determinant = matrix.computeDeterminant();
+      assertThat(determinant, is(-2.0));
+  }
+
+  public void testComputeEigenvalues() {
+      double[] eigenvalues = matrix.computeEigenvalues(100, 1e-10);
+      assertThat(Double.valueOf(eigenvalues[0]*10000).intValue(), is(53722));
+      assertThat(Double.valueOf(eigenvalues[1]*10000).intValue(), is(-3722));
+  }
+}
