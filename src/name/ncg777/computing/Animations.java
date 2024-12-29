fa67df2b2b2a86bcaeaab2dda886fbd4a5885994
Nicolas Couture-Grenier <nicolas.couture.grenier@gmail.com>
2024-12-28 21:59:46 -0500

    ...

--- src/name/ncg777/computing/Animations.java
+++ src/name/ncg777/computing/Animations.java
@@ -160,8 +160,8 @@ public class Animations {
                 double th = Math.atan2(_x, _y);
                 Double r = Math.sqrt((Math.pow(_x, 2.0) + Math.pow(_y, 2.0))/2.0);
                 
-                Double x = r*Math.cos(th+2.0*2.0*Math.PI*_f.apply(r));
-                Double y = r*Math.sin(th+2.0*2.0*Math.PI*_f.apply(r));
+                Double x = r*Math.cos(th+Math.PI*(-1.0+2.0*_f.apply(r)));
+                Double y = r*Math.sin(th+Math.PI*(-1.0+2.0*_f.apply(r)));
                 
                 double v = m.get(
                     (int)((0.5+x*0.5)*(double)(dim-1)), 
