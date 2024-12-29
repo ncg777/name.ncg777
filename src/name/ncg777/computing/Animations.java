bdb66c73c3ec732393b9e4388b861a4ea42e6613
Nicolas Couture-Grenier <nicolas.couture.grenier@gmail.com>
2024-12-28 21:19:28 -0500

    ...

--- src/name/ncg777/computing/Animations.java
+++ src/name/ncg777/computing/Animations.java
@@ -154,14 +154,14 @@ public class Animations {
           var img = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
           var g = img.createGraphics();
           int dim = m.columnCount();
-          g.rotate(Math.PI/4.0+_f.apply(t)*Math.PI*2.0,width/2,height/2);
+          g.rotate(Math.PI/4.0-_f.apply(t)*Math.PI*2.0,width/2,height/2);
           GraphicsFunctions.drawColorField2D(g, 
               (_x,_y) -> {
                 double th = Math.atan2(_x, _y);
                 Double r = Math.sqrt((Math.pow(_x, 2.0) + Math.pow(_y, 2.0))/2.0);
                 
-                Double x = r*Math.sin(th+Math.PI*_f.apply(r));
-                Double y = r*Math.cos(th+Math.PI*_f.apply(r));
+                Double x = r*Math.sin(th+2.0*Math.PI*_f.apply(r));
+                Double y = r*Math.cos(th+2.0*Math.PI*_f.apply(r));
                 
                 //System.out.println(Double.toString(x) + ", " + Double.toString(y));
                 double v = m.get(
